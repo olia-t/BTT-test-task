4857c53b9283a1a4ea12908474494dad33fe640e
olia-t <olha.tokarieva.i@gmail.com>
2023-09-18 10:41:14 +0300

    create storage acc and container

--- terraform-ci/backend.tf
+++ terraform-ci/backend.tf
@@ -1,19 +1,16 @@
-# resource "azurerm_storage_account" "storage-bett" {
-#   name                     = var.storage_account_name
-#   resource_group_name      = var.resource_group_name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   allow_blob_public_access = true
-#   enable_blob_encryption   = true
+resource "azurerm_storage_account" "storage-bett" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-# }
+}
 
-# resource "azurerm_storage_container" "container-storage-bett" {
-#   name                  = var.storage_container_name
-#   storage_account_name  = var.storage_account_name
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_container" "container-storage-bett" {
+  name                  = var.storage_container_name
+  storage_account_name  = var.storage_account_name
+}
 
 ###############################################################################
 ## template
